249b879350480a7892cbb9f1e74cfd1fdc00e838
chika-kkk <pinavt4253@gmail.com>
2026-03-21 17:41:33 +0900

    変更

--- inquiry-app.js
+++ inquiry-app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const client = new kintoneModule.KintoneRestAPIClient({
-	baseUrl: `https://${process.env.KINTONE_DOMAIN}/k/v1/record.json`,
+	baseUrl: `https://${process.env.KINTONE_DOMAIN}`,
 	auth: {
 		apiToken: process.env.KINTONE_API_TOKEN,
 	}
